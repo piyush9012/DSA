f4bd3dbb90d8e99daa22fdf1daa541f6b2b9658c
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-04-27 23:46:02 +0530

    Minimum days to make M bouquets

---
+++ BinarySearch/bouquet.java
@@ -0,0 +1,54 @@
+// Problem Statement: You are given 'N’ roses and you are also given an array 'arr'  where 'arr[i]' denotes that the 'ith' rose will bloom on the 'arr[i]th' day. You can only pick already bloomed roses that are adjacent to make a bouquet. You are also told that you require exactly 'k' adjacent bloomed roses to make a single bouquet. Find the minimum number of days required to make at least ‘m' bouquets each containing 'k' roses. Return -1 if it is not possible.
+
+import java.util.*;
+
+public class bouquet {
+    public static boolean possible(int arr[], int day, int m, int k){
+        int n = arr.length;
+        int count = 0, noOfB = 0;
+        for(int i=0; i < n; i++){
+            if(arr[i] <= day){
+                count++;
+            }
+            else{
+                noOfB += count/k;
+                count++;
+            }
+        }
+        noOfB += count/k;
+        return noOfB >= m;
+    }
+
+    public static int roseGarden(int[] arr, int k, int m) {
+        long val = (long) m * k;
+        int n = arr.length;
+        if (val > n) return -1;
+        
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, arr[i]);
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(arr, mid, m, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+    public static void main(String[] args) {
+        int[] arr = {7, 7, 7, 7, 13, 11, 12, 7};
+        int k = 3;
+        int m = 2;
+        int ans = roseGarden(arr, k, m);
+        if (ans == -1)
+            System.out.println("We cannot make m bouquets.");
+        else
+            System.out.println("We can make bouquets on day " + ans);
+    }
+}
