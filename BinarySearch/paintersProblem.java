eeece7a0ef167e51620f00eb744962bab336ae48
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-06-02 13:50:09 +0530

    Painter's Partition Problem

--- BinarySearch/paintersProblem.java
+++ BinarySearch/paintersProblem.java
@@ -1,26 +1,29 @@
 // Problem Statement: Given an array/list of length ‘N’, where the array/list represents the boards and each element of the given array/list represents the length of each board. Some ‘K’ numbers of painters are available to paint these boards. Consider that each unit of a board takes 1 unit of time to paint. You are supposed to return the area of the minimum time to get this job done of painting all the ‘N’ boards under the constraint that any painter will only paint the continuous sections of boards.
 
-import java.util.*;
-
 public class Solution {
-    public static int countPainters(ArrayList<Integer> boards, int time) {
-        int n = boards.size(); // size of array.
+    public static int countPainters(int boards, int time) {
+        int n = boards.length;
         int painters = 1;
         long boardsPainter = 0;
         for (int i = 0; i < n; i++) {
-            if (boardsPainter + boards.get(i) <= time) {
-                boardsPainter += boards.get(i);
+            if (boardsPainter + boards[i] <= time) {
+                boardsPainter += boards[i];
             } else {
                 painters++;
-                boardsPainter = boards.get(i);
+                boardsPainter = boards[i];
             }
         }
         return painters;
     }
 
-    public static int findLargestMinDistance(ArrayList<Integer> boards, int k) {
-        int low = Collections.max(boards);
-        int high = boards.stream().mapToInt(Integer::intValue).sum();
+    public static int findLargestMinDistance(int boards, int k) {
+        int low = boards[0];
+        int high = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            low = Math.max(low, a[i]);
+            high += a[i];
+        }
 
         while (low <= high) {
             int mid = (low + high) / 2;
@@ -35,7 +38,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> boards = new ArrayList<>(Arrays.asList(10, 20, 30, 40));
+        int boards[] = {10, 20, 30, 40};
         int k = 2;
         int ans = findLargestMinDistance(boards, k);
         System.out.println("The answer is: " + ans);
